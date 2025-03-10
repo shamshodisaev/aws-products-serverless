26e8a8dd33b0f0727157ff89506ce7536383ed7a
shamshodisaev <shamshod_isaev@epam.com>
2025-03-10 06:19:14 +0500

    feature: add product

--- backend/product-service/lambda/product.ts
+++ backend/product-service/lambda/product.ts
@@ -1,29 +1,68 @@
-import { EventSourceMapping } from "aws-cdk-lib/aws-lambda";
-const products = require("./products.json");
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { corsHeaders } from "./product-utils";
+
+const headers = {
+  "Content-Type": "application/json",
+  ...corsHeaders,
+};
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+const dynamo = DynamoDBDocumentClient.from(client);
 
 exports.handler = async (event: any) => {
   try {
     const productId = event.pathParameters.id;
 
-    const product = products.find((product: any) => product.id === productId);
+    const { Items: productItems } = await dynamo.send(
+      new QueryCommand({
+        TableName: process.env.PRODUCTS_TABLE,
+        KeyConditionExpression: "#id = :idVal",
+        ExpressionAttributeNames: {
+          "#id": "id",
+        },
+        ExpressionAttributeValues: {
+          ":idVal": productId,
+        },
+      })
+    );
 
-    if (!product) {
+    if (!productItems?.length) {
       return {
         statusCode: 404,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ data: product }),
+        headers,
+        body: JSON.stringify({ message: "Not found!" }),
       };
     }
 
+    let price = undefined;
+
+    try {
+      const { Items } = await dynamo.send(
+        new QueryCommand({
+          TableName: process.env.PRODUCT_STOCKS_TABLE,
+          KeyConditionExpression: "product_id = :productId",
+          ExpressionAttributeValues: {
+            ":productId": productItems?.[0].id,
+          },
+        })
+      );
+      price = Items?.[0]?.price;
+    } catch (error) {
+      console.log(error, "error");
+    }
+
+    const priceObj = price ? { price } : {};
+
     return {
       statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ data: product }),
+      headers,
+      body: JSON.stringify({ data: { ...productItems, ...priceObj } }),
     };
   } catch (error) {
     return {
       statusCode: 500,
-      headers: { "Content-Type": "application/json" },
+      headers,
       body: JSON.stringify({ error }),
     };
   }
