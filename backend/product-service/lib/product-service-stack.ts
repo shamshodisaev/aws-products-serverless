9c9faeaff66c10d51a527e74b264fbacfed7edd9
shamshodisaev <shamshod_isaev@epam.com>
2025-03-10 06:21:07 +0500

    feature: update stack

--- backend/product-service/lib/product-service-stack.ts
+++ backend/product-service/lib/product-service-stack.ts
@@ -2,23 +2,70 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+const environment = {
+  PRODUCTS_TABLE: "products",
+  PRODUCT_STOCKS_TABLE: "product-stocks",
+};
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const dynamoDbPolicy = new iam.PolicyStatement({
+      actions: [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:Scan",
+        "dynamodb:Query",
+        "dynamodb:UpdateItem",
+        "dynamodb:DeleteItem",
+      ],
+      resources: [
+        `arn:aws:dynamodb:us-east-1:311141532338:table/${environment.PRODUCTS_TABLE}`,
+        `arn:aws:dynamodb:us-east-1:311141532338:table/${environment.PRODUCT_STOCKS_TABLE}`,
+      ],
+    });
+
+    const lambdaRole = new iam.Role(this, "LambdaDynamoDBRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    lambdaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+    lambdaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaVPCAccessExecutionRole"
+      )
+    );
+    lambdaRole.addToPolicy(dynamoDbPolicy);
 
     const getProductsList = new lambda.Function(this, "ProductsList", {
       runtime: lambda.Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset("lambda"),
       handler: "products.handler",
+      environment,
+      role: lambdaRole,
+    });
+
+    const createProduct = new lambda.Function(this, "CreateProduct", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "create-product.handler",
+      environment,
+      role: lambdaRole,
     });
 
     const getProductById = new lambda.Function(this, "Product", {
       runtime: lambda.Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset("lambda"),
       handler: "product.handler",
+      environment,
+      role: lambdaRole,
     });
 
     const productApi = new apigateway.RestApi(this, "Products API", {
@@ -31,9 +78,11 @@ export class ProductServiceStack extends cdk.Stack {
     const productsListLambda = new apigateway.LambdaIntegration(
       getProductsList
     );
+    const createProductLambda = new apigateway.LambdaIntegration(createProduct);
     const productByIdLambda = new apigateway.LambdaIntegration(getProductById);
 
     productsResource.addMethod("GET", productsListLambda);
+    productsResource.addMethod("POST", createProductLambda);
     productResource.addMethod("GET", productByIdLambda);
   }
 }
