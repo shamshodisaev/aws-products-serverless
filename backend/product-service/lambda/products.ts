070c204064b94ff2a5a961f4a8c20230c61906fc
shamshodisaev <shamshod_isaev@epam.com>
2025-03-10 06:20:05 +0500

    feature: integrate dynamodb to products list

--- backend/product-service/lambda/products.ts
+++ backend/product-service/lambda/products.ts
@@ -1,10 +1,54 @@
+import {
+  DynamoDBClient,
+  ScanCommand,
+} from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 import { type EventSourceMapping } from "aws-cdk-lib/aws-lambda";
-const products = require("./products.json");
+import { corsHeaders } from "./product-utils";
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+const dynamo = DynamoDBDocumentClient.from(client);
 
 exports.handler = async (event: EventSourceMapping) => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ data: products }),
-  };
+  try {
+    const { Items: productsData } = await dynamo.send(
+      new ScanCommand({ TableName: process.env.PRODUCTS_TABLE })
+    );
+
+    const { Items: stocksData } = await dynamo.send(
+      new ScanCommand({ TableName: process.env.PRODUCT_STOCKS_TABLE })
+    );
+
+    const products = productsData?.map((product) => {
+      const productStock = stocksData?.find(
+        (stock) => stock.product_id.S === product.id.S
+      );
+
+      return {
+        ...product,
+        ...(productStock?.price ? { price: productStock?.price } : {}),
+      };
+    });
+
+    const normalizedProducts = products?.map((product) => unmarshall(product));
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        ...corsHeaders,
+      },
+      body: JSON.stringify({ data: normalizedProducts }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        ...corsHeaders,
+      },
+      body: JSON.stringify({ message: "Something wrong happened!" }),
+    };
+  }
 };
